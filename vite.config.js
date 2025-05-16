a210dc94bec99d805910c229b56dd5314893e704
Alexmineev <mineev1981@gmail.com>
2025-05-16 10:45:22 +0200

    Added scss

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  build: {
-    outDir: '../dist'
-  }
-})
+    root: './src',
+    server: {
+        port: 3000,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
+});
