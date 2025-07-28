f47f89d5a45c93beba13cd2d0d7f647ff30814ae
Alexmineev <mineev1981@gmail.com>
2025-07-28 16:04:07 +0200

    минификация и сборка html и images

--- eslint.config.js
+++ eslint.config.js
@@ -7,42 +7,43 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  // Основные настройки для браузерного JS
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-      },
+    // Основные настройки для браузерного JS
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            ...eslintConfigPrettier.rules,
+        },
     },
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      ...eslintConfigPrettier.rules,
-    },
-  },
 
-  // Подключаем рекомендованные правила ESLint
-  pluginJs.configs.recommended,
+    // Подключаем рекомендованные правила ESLint
+    pluginJs.configs.recommended,
 
-  // Отдельный блок для Node.js конфигов и скриптов
-  {
-    files: ['vite.config.js', '.eslintrc.{js,cjs}'],
-    languageOptions: {
-      sourceType: 'script',
-      globals: {
-        ...globals.node,
-      },
+    // Отдельный блок для Node.js конфигов и скриптов
+    {
+        files: ['vite.config.js', '.eslintrc.{js,cjs}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module', // ← здесь должно быть 'module', а не 'script'
+            globals: {
+                ...globals.node,
+            },
+        },
     },
-  },
 
-  // Исключаем ненужные папки из линтинга
-  {
-    ignores: ['node_modules', 'dist'],
-  },
+    // Исключаем ненужные папки из линтинга
+    {
+        ignores: ['node_modules', 'dist'],
+    },
 ]);
